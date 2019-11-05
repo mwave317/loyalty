5723417a0afafeead67f5a6fdd37e6f82b7cbd87
Ken Coppola <kcoppola@ranker.com>
2019-11-05 13:22:43 -0500

    Compelted the loyalty project.

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import React from 'react';
+import './App.css';
+import purchases  from './data';
+
+function App() {
+const customersTotalPoints = [];
+
+
+  let points = (total) => {
+    let totalPointsEarned = 0;
+    if (total > 50 && total < 100) {
+        return totalPointsEarned = total - 50;
+    } else {
+        totalPointsEarned = Math.floor(total - 100) * 2 + 50;
+        return  totalPointsEarned;
+    }
+}
+
+   let createTable = (points) => {
+        if (points) {
+        points.map(customer => {
+                return <div><p>{customer.customerName}</p><p>{customer.customerNumber}{customer.totalPoints}</p></div>
+            });
+        }
+
+    }
+
+  let enableSort = (a, b) => {
+    if (a.customerNumber < b.customerNumber) {
+        return -1;
+    }
+
+    if (a.customerNumber > b.customerNumber) {
+        return 1;
+    }
+
+    return 0;
+}; 
+
+let calculateTotals = (sortedArray) => {
+  let totalPoints = 0;
+  let pointTotals = {};
+//   let customersTotalPoints = [];
+
+  let sortedByCustomerNumber = sortedArray.sort(enableSort);
+ 
+  for (let i = 0; i < sortedByCustomerNumber.length; i++) {
+      totalPoints = sortedByCustomerNumber[i].pointsAwarded;
+      let index = 0;
+
+      while (index <= sortedByCustomerNumber.length) {
+          let counter = 0;
+
+          while (sortedByCustomerNumber[index].customerNumber === sortedByCustomerNumber[i].customerNumber) {
+              totalPoints += sortedByCustomerNumber[i++].pointsAwarded;
+              counter++;
+
+              if (i === sortedByCustomerNumber.length) {
+                  pointTotals = { customerName: sortedByCustomerNumber[i - 1].customerName, customerNumber: sortedByCustomerNumber[i - 1].customerNumber, totalPoints };
+                  customersTotalPoints.push(pointTotals);
+                  createTable(customersTotalPoints);
+                  return 
+              }
+          }
+          pointTotals = { customerName: sortedByCustomerNumber[i - 1].customerName, customerNumber: sortedByCustomerNumber[i - 1].customerNumber, totalPoints };
+          customersTotalPoints.push(pointTotals);
+          totalPoints = 0;
+          index += counter;
+      }
+  }
+}
+
+let sortTheArray = () => {
+
+  let sortedArray = [];
+  let sortedAndFlat = [];
+  
+  // Add the points and sort the array by Customer Name
+  
+  
+  for (let purchase of purchases){ 
+  
+      for ( let month in purchase) {
+          for (let getTotal in purchase[month]){
+              purchase[month][getTotal].pointsAwarded = points(parseInt(purchase[month][getTotal].total.replace(/\$/g, '')));
+          }
+  
+           let sorted = purchase[month].sort(enableSort);
+              sortedArray.push(sorted);    
+           
+      };
+  };
+  
+  sortedAndFlat = [].concat.apply([], sortedArray);
+  calculateTotals(sortedAndFlat);
+  
+  }
+
+  const columns = ['Customer Number', 'Customer Name', 'Total Points'];
+  
+
+  
+  
+  sortTheArray();
+
+   
+    
+  return (
+  
+     <div class = "table">
+         <div class="columns"><p class=" columns">Customer Number</p><p class="columns">Customer Name</p><p class="columns">Total Points</p></div>
+         <div>{customersTotalPoints.map(customer => {
+        return <div class= "data"><p>{customer.customerNumber}</p><p>{customer.customerName}</p><p>{customer.totalPoints}</p></div>
+    })}</div>
+     </div>
+  );
+
+  }
+export default App;
---
+++ src/data.js
@@ -0,0 +1,181 @@
+let purchases = [{
+    October: [{
+        tranactionID: 1,
+        date: '10/01/2019',
+        customerNumber: '0000987641532',
+        customerName: 'Jane Doe',
+        total: '$120.00',
+    },
+    {
+        transactionID: 2,
+        date: '10/01/2019',
+        customerNumber: '0000987641533',
+        customerName: 'Sherry Miller',
+        total: '$130.00',
+    },
+    {
+        transactionID: 3,
+        date: '10/03/2019',
+        customerNumber: '0000987641534',
+        customerName: 'Mandy DeLuca',
+        total: '185.00'
+    },
+    {
+        tranactionID: 4,
+        date: '10/04/2019',
+        customerNumber: '0000987641532',
+        customerName: 'Jane Doe',
+        total: '$263.00',
+    },
+    {
+        transactionID: 5,
+        date: '10/05/2019',
+        customerNumber: '0000987641533',
+        customerName: 'Sherry Miller',
+        total: '$195.00',
+    },
+    {
+        transactionID: 6,
+        date: '10/08/2019',
+        customerNumber: '0000987641534',
+        customerName: 'Mandy DeLuca',
+        total: '135.00'
+    },
+    {
+        transactionID: 7,
+        date: '10/11/2019',
+        customerNumber: '0000987641534',
+        customerName: 'Mandy DeLuca',
+        total: '415.00'
+    },
+    ],
+    November: [{
+        tranactionID: 1,
+        date: '11/14/2019',
+        customerNumber: '0000987641532',
+        customerName: 'Jane Doe',
+        total: '$150.00',
+    },
+    {
+        transactionID: 2,
+        date: '11/15/2019',
+        customerNumber: '0000987641533',
+        customerName: 'Sherry Miller',
+        total: '$170.00',
+    },
+    {
+        transactionID: 3,
+        date: '11/18/2019',
+        customerNumber: '0000987641534',
+        customerName: 'Mandy DeLuca',
+        total: '195.00'
+    },
+    {
+        tranactionID: 4,
+        date: '11/22/2019',
+        customerNumber: '0000987641532',
+        customerName: 'Jane Doe',
+        total: '$75.00',
+    },
+    {
+        transactionID: 5,
+        date: '11/24/2019',
+        customerNumber: '0000987641533',
+        customerName: 'Sherry Miller',
+        total: '$112.00',
+    },
+    {
+        transactionID: 6,
+        date: '11/26/2019',
+        customerNumber: '0000987641534',
+        customerName: 'Mandy DeLuca',
+        total: '294.00'
+    },
+     {
+        transactionID: 7,
+        date: '11/28/2019',
+        customerNumber: '0000987641532',
+        customerName: 'Jane Doe',
+        total: '64.00'
+    },
+    ],
+    December: [{
+        tranactionID: 1,
+        date: '12/01/2019',
+        customerNumber: '0000987641532',
+        customerName: 'Jane Doe',
+        total: '$120.00',
+    },
+    {
+        transactionID: 2,
+        date: '12/02/2019',
+        customerNumber: '0000987641533',
+        customerName: 'Sherry Miller',
+        total: '$170.00',
+    },
+    {
+        transactionID: 3,
+        date: '12/05/2019',
+        customerNumber: '0000987641534',
+        customerName: 'Mandy DeLuca',
+        total: '185.00'
+    },
+    {
+        tranactionID: 4,
+        date: '12/06/2019',
+        customerNumber: '0000987641532',
+        customerName: 'Jane Doe',
+        total: '$150.00',
+    },
+    {
+        transactionID: 5,
+        date: '12/14/2019',
+        customerNumber: '0000987641533',
+        customerName: 'Sherry Miller',
+        total: '$170.00',
+    },
+    {
+        transactionID: 6,
+        date: '12/18/2019',
+        customerNumber: '0000987641534',
+        customerName: 'Mandy DeLuca',
+        total: '185.00'
+    },
+     {
+        transactionID: 7,
+        date: '12/19/2019',
+        customerNumber: '0000987641532',
+        customerName: 'Jane Doe',
+        total: '185.00'
+    },
+    {
+        tranactionID: 8,
+        date: '12/21/2019',
+        customerNumber: '0000987641532',
+        customerName: 'Jane Doe',
+        total: '$150.00',
+    },
+    {
+        transactionID: 9,
+        date: '12/21/2019',
+        customerNumber: '0000987641533',
+        customerName: 'Sherry Miller',
+        total: '$170.00',
+    },
+    {
+        transactionID: 10,
+        date: '12/22/2019',
+        customerNumber: '0000987641534',
+        customerName: 'Mandy DeLuca',
+        total: '185.00'
+    },
+     {
+        transactionID: 11,
+        date: '12/24/2019',
+        customerNumber: '0000987641532',
+        customerName: 'Jane Doe',
+        total: '105.00'
+    }]
+}];
+
+export default purchases;
